73e59c01926efb5d58788a5538a70b933b17880b
gkoukis <127508084+gkoukis@users.noreply.github.com>
2024-09-26 11:01:27 +0000

    remove pods after experiments for ../cni and better delete if needed

--- examples/workloads/kubelet-density-heavy/delete-all-deployments-pods.sh
+++ examples/workloads/kubelet-density-heavy/delete-all-deployments-pods.sh
@@ -5,40 +5,39 @@ start=0
 end=10
 namespace="kubelet-density-heavy"
 
-for i in $(seq $start $end); do
-    #deployment_name="webserver-1-$i"
-    #deployment_name="curl-1-$i"
-    #deployment_name="perfapp-1-$i"
-    #deployment_name="postgres-1-$i"
-    #deployment_name="curl-$i-0"
-    deployment_name="perfapp-$i-0"
-    #deployment_name="postgres-$i-0"
-    echo "Scaling down deployment: $deployment_name"
+# Array of deployment name patterns
+deployment_names=("perfapp-" "postgres-")
 
-    # Scale down the deployment to 0 replicas
-    kubectl scale --replicas=0 deployment/$deployment_name -n $namespace
+for deployment_name_prefix in "${deployment_names[@]}"; do
+    for i in $(seq $start $end); do
+        deployment_name="${deployment_name_prefix}${i}-0"
+        echo "Scaling down deployment: $deployment_name"
 
-    echo "Deleting deployment: $deployment_name"
-    # Delete the deployment to ensure no pods are redeployed
-    kubectl delete deployment $deployment_name -n $namespace
+        # Scale down the deployment to 0 replicas
+        kubectl scale --replicas=0 deployment/$deployment_name -n $namespace
 
-    echo "Deleting any remaining pods for deployment: $deployment_name"
-    # Force delete all pods that start with the pod_name
-    kubectl get pods -n $namespace --no-headers | awk '{print $1}' | grep "^$deployment_name-" | xargs -r -I {} kubectl delete pod {} -n $namespace --grace-period=0 --force
+        echo "Deleting deployment: $deployment_name"
+        # Delete the deployment to ensure no pods are redeployed
+        kubectl delete deployment $deployment_name -n $namespace
 
-    # Wait for a moment to allow command to send
-    sleep 2
+        echo "Deleting any remaining pods for deployment: $deployment_name"
+        # Force delete all pods that start with the deployment_name
+        kubectl get pods -n $namespace --no-headers | awk '{print $1}' | grep "^$deployment_name-" | xargs -r -I {} kubectl delete pod {} -n $namespace --grace-period=0 --force
 
-    # Check if any pods with the name still exist and attempt to remove finalizers if necessary
-    remaining_pods=$(kubectl get pods -n $namespace --no-headers | awk '{print $1}' | grep "^$deployment_name-")
-    if [ ! -z "$remaining_pods" ]; then
-        echo "Some pods still exist, attempting to remove finalizers..."
-        for pod in $remaining_pods; do
-            kubectl patch pod $pod -n $namespace --type=json -p='[{"op": "remove", "path": "/metadata/finalizers"}]'
-        done
-    fi
+        # Wait for a moment to allow command to send
+        sleep 2
 
-    echo "Deployment $deployment_name and its pods handled."
+        # Check if any pods with the name still exist and attempt to remove finalizers if necessary
+        remaining_pods=$(kubectl get pods -n $namespace --no-headers | awk '{print $1}' | grep "^$deployment_name-")
+        if [ ! -z "$remaining_pods" ]; then
+            echo "Some pods still exist, attempting to remove finalizers..."
+            for pod in $remaining_pods; do
+                kubectl patch pod $pod -n $namespace --type=json -p='[{"op": "remove", "path": "/metadata/finalizers"}]'
+            done
+        fi
+
+        echo "Deployment $deployment_name and its pods handled."
+    done
 done
 
-echo "All specified deployments and their pods have been processed."
+echo "All deployments and pods have been processed."
